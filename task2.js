c67a753d96d5d093089aa5045828143285dabac0
Muhamad Iqbal Aprido <Edo.vivoy21@gmail.com>
2023-01-03 20:56:55 +0700

    Update task2.js

--- task2.js
+++ task2.js
@@ -13,15 +13,26 @@
 // Output:
 // [“Alexandra”,”Amanda”,”Angela”]
 
-const name = ['Abigail', 'Alexandra', 'Alison', 'Amanda', 'Angela', 'Bella', 'Carol', 'Caroline', 'Carolyn', 'Deirdre', 'Diana', 'Elizabeth', 'Ella', 'Faith', 'Olivia', 'Penelope'];
+const nama = ['Abigail', 'Alexandra', 'Alison', 'Amanda', 'Angela', 'Bella', 'Carol', 'Caroline', 'Carolyn', 'Deirdre', 'Diana', 'Elizabeth', 'Ella', 'Faith', 'Olivia', 'Penelope'];
 
-function searchName(subString, limit, callback) {
-  let newName = name
-    .filter((string) => string.toLowerCase().includes(subString.toLowerCase())) // filter pada variabel name
-    .filter((string, index) => index < limit); // filter pada array baru setelah difilter awal
-  callback(newName);
+function cekData(string, number, callback) {
+  try {
+    if (typeof string != 'string') throw 'inputkan data berupa string';
+    filterData(string, number, callback);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-searchName('an', 3, (data) => {
-  console.log(data); // contoh callback
-});
+function filterData(string, number, callback) {
+  let result = nama.filter((nama) => nama.toLowerCase().indexOf(string.toLowerCase()) !== -1);
+  result = result.slice(0, number);
+  callback(result); //revisi, solved
+}
+
+function callback(result) {
+  console.log(result);
+}
+
+cekData('an', 3, callback);
+// cekData(4, 3, callback);
